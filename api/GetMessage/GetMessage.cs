c70a759b1104169c2a8ab291921d01183e5489fe
chao <wang>
2020-07-09 16:49:06 +0900

    a

--- api/GetMessage/GetMessage.cs
+++ api/GetMessage/GetMessage.cs
@@ -10,7 +10,7 @@ using Newtonsoft.Json;
 
 namespace GetMessage
 {
-    public static class Function1
+    public static class GetMessage
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
